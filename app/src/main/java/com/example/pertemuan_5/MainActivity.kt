d87c4c869aa72e8e525778202a5ed1eb207043b8
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-11-02 13:45:23 +0700

    perbaikan mainactivity agar bisa melihat tampilan emulator

--- app/src/main/java/com/example/pertemuan_5/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan_5/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan_5Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
